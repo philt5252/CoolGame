23e4a9d93700a9001db1fb88b97d328806669dab
philt5252 <philt5252@gmail.com>
2013-10-28 19:44:31 -0500

    Added other game and GameObject

---
+++ OtherGame/Level1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GameEngine;
+
+namespace OtherGame
+{
+    public partial class Level1 : GameLevel
+    {
+        public Level1()
+        {
+            InitializeComponent();
+        }
+    }
+}
